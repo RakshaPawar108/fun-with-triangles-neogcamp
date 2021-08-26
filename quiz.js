f6e78e9beefbb83fde1c5944f5315514dc7b455b
Raksha Pawar <rakshapawar10.sae@gmail.com>
2021-08-26 12:38:47 +0530

    feat: functionality of triangle quiz added.

---
+++ quiz.js
@@ -0,0 +1,21 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitBtn = document.querySelector("#submit-answer-btn");
+const outputDiv = document.querySelector("#output");
+
+const correctAnswers = ["90", "right"];
+
+function calcScore() {
+  let score = 0;
+  let arrIndex = 0;
+  const formResults = new FormData(quizForm);
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[arrIndex]) {
+      score += 1;
+    }
+    arrIndex += 1;
+  }
+
+  outputDiv.innerText = "Your score is " + score;
+}
+
+submitBtn.addEventListener("click", calcScore);
